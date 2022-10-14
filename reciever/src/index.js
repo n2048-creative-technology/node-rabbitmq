7a2c270ae8671de62503e87f7c3bed7edbaf0241
n2048-creative-technology <56191120+n2048-creative-technology@users.noreply.github.com>
2022-10-14 23:32:56 +0200

    Update index.js

--- reciever/src/index.js
+++ reciever/src/index.js
@@ -1,8 +1,9 @@
 import rabbit from './rabbit'
+import config from 'config'
 
 rabbit.getInstance()
   .then(broker => {
-    broker.subscribe('test', (msg, ack) => {
+    broker.subscribe(config.rabbit.queue, (msg, ack) => {
       console.log('Message:', msg.content.toString())
       ack()
     })
