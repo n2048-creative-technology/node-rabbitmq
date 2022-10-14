c22d2a832637fc9b03a0b511f2026e31a5b23681
n2048-creative-technology <56191120+n2048-creative-technology@users.noreply.github.com>
2022-10-14 23:30:15 +0200

    Update send.js

--- sender/src/routes/public/send.js
+++ sender/src/routes/public/send.js
@@ -1,9 +1,14 @@
 import * as rabbit from '../../rabbit'
+import config from 'config'
+import { v4 as uuidv4 } from 'uuid';
 
 const send = async (ctx) => {
-  const broker = await rabbit.getInstance()
-  await broker.send('test', Buffer.from(JSON.stringify(ctx.request.body)))
-  ctx.body = ''
+  const broker = await rabbit.getInstance();
+  const queue = config.rabbit.queue; 
+  const json = {...ctx.request.body, id: uuidv4()};
+  const msg = JSON.stringify(json);
+  await broker.send(queue, Buffer.from(msg));
+  ctx.body = '';
 }
 
 export default {
